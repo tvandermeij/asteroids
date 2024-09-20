d4a74dd45e99dc0b117720fc7489a4200d20cd61
Soulhaxs <t.vdmeij@outlook.com>
2024-09-20 14:51:26 +0200

    E: add asteroids on screen

--- main.py
+++ main.py
@@ -4,13 +4,29 @@
 import pygame
 from constants import *
 from player import *
+from asteroidsfield import *
 
 def main():
     pygame.init()
     time = pygame.time.Clock()
     dt = 0
+
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Player.containers = (updateable, drawable)
+    Asteroid.containers = (asteroids, updateable, drawable)
+    AsteroidField.containers = (updateable)
+
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    AsteroidField()
+
+    print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -18,20 +34,16 @@ def main():
 
 
         dt = time.tick(60) / 1000 
-        player.update(dt)
+
+        for item in updateable:
+            item.update(dt)
         
         pygame.Surface.fill(screen, "black")
-        player.draw(screen)
+
+        for item in drawable:
+            item.draw(screen)
 
         pygame.display.flip()
-
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-
-
-
-
 
 if __name__ == "__main__":
     main()
