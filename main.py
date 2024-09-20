69d7009739866e58e271ffeba41b4ffbd0bc9f48
Soulhaxs <t.vdmeij@outlook.com>
2024-09-20 11:35:18 +0200

    C: add player with horizontal rotation

--- main.py
+++ main.py
@@ -3,20 +3,33 @@
 # throughout this file
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
+    time = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+
+        dt = time.tick(60) / 1000 
+        player.update(dt)
+        
         pygame.Surface.fill(screen, "black")
+        player.draw(screen)
+
         pygame.display.flip()
-    
+
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+
 
 
 
